119640b22e8dd03990ce746beb42c6d3e9df7e55
badwambad <sobadwambad@proton.me>
2022-12-15 11:16:05 -0300

    LensUser. Add token estimation in USD

--- lenses/LensUser.sol
+++ lenses/LensUser.sol
@@ -50,9 +50,11 @@ contract LensUser {
     address internal constant WOM_TOKEN = 0xAD6742A35fB341A9Cc6ad674738Dd8da98b94Fb1;
     address internal constant BUSD_TOKEN = 0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56;
     address internal constant WBNB_TOKEN = 0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c;
+    address internal constant SD_TOKEN = 0x3BC5AC0dFdC871B365d159f728dd1B9A0B5481E8;
     address internal constant WMX_WOM_TOKEN = 0x0415023846Ff1C6016c4d9621de12b24B2402979;
 
     address internal constant PANCAKE_ROUTER = 0x10ED43C718714eb63d5aA57B78B54704E256024E;
+    address internal constant APE_ROUTER = 0xcF0feBd3f17CEf5b47b0cD257aCf6025c5BFf3b7;
 
     function getUserBalancesDefault(
         IBooster _booster,
@@ -112,21 +114,20 @@ contract LensUser {
         uint256[] memory underlyingBalances,
         uint256[] memory usdOuts,
         address[][] memory rewardTokens,
-        uint256[][] memory earnedRewards
+        uint256[][] memory earnedRewardsUSD
     ) {
         uint256 len = _poolIds.length;
         lpTokenBalances = new uint256[](len);
         underlyingBalances = new uint256[](len);
         usdOuts = new uint256[](len);
         rewardTokens = new address[][](len);
-        earnedRewards = new uint256[][](len);
+        earnedRewardsUSD = new uint256[][](len);
 
         for (uint256 i = 0; i < len; i++) {
             IBooster.PoolInfo memory poolInfo = _booster.poolInfo(_poolIds[i]);
 
             // 1. Earned rewards
-            (rewardTokens[i], earnedRewards[i]) = IBaseRewardPool4626(poolInfo.crvRewards)
-                .claimableRewards(_user);
+            (rewardTokens[i],,earnedRewardsUSD[i]) = getUserPendingRewards(poolInfo.crvRewards, _user);
 
             // 2. LP token balance
             uint256 lpTokenBalance = IERC20(poolInfo.crvRewards).balanceOf(_user);
@@ -181,6 +182,49 @@ contract LensUser {
         } else {
             revert("unsupported pool");
         }
+    }
+
+    function getUserPendingRewards(address _rewardsPool, address _user)
+        public view returns (
+            address[] memory rewardTokens,
+            uint256[] memory earnedRewards,
+            uint256[] memory earnedRewardsUSD
+    ) {
+        (rewardTokens, earnedRewards) = IBaseRewardPool4626(_rewardsPool)
+            .claimableRewards(_user);
+
+        earnedRewardsUSD = new uint256[](rewardTokens.length);
+        for (uint256 i = 0; i < earnedRewards.length; i++) {
+            if (earnedRewards[i] > 0) {
+                earnedRewardsUSD[i] = estimateInUSD(rewardTokens[i], earnedRewards[i]);
+            }
+        }
+    }
+
+    function estimateInUSD(address _token, uint256 _amountIn) public view returns (uint256) {
+        address router = PANCAKE_ROUTER;
+        bool throughBnb = true;
+        if (_token == SD_TOKEN) {
+            router = APE_ROUTER;
+            throughBnb = false;
+        }
+        if (_token == WOM_TOKEN) {
+            throughBnb = false;
+        }
+
+        address[] memory path;
+        if (throughBnb) {
+            path = new address[](3);
+            path[0] = _token;
+            path[1] = WBNB_TOKEN;
+            path[2] = BUSD_TOKEN;
+        } else {
+            path = new address[](2);
+            path[0] = _token;
+            path[1] = BUSD_TOKEN;
+        }
+        uint256[] memory amountsOut = IUniswapV2Router01(router).getAmountsOut(_amountIn, path);
+        return amountsOut[amountsOut.length - 1];
     }
 
     // OTHER HELPERS
