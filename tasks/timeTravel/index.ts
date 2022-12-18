fbb2c5113d4e152ca101ff8b4c7bdf1e024e8519
daohacker <daohacker@proton.me>
2022-12-18 16:00:31 +0800

    tt:wombat:pool

--- tasks/timeTravel/index.ts
+++ tasks/timeTravel/index.ts
@@ -1,6 +1,9 @@
 import { task } from "hardhat/config";
 import { TaskArguments } from "hardhat/types";
 import { HardhatRuntime } from "../utils/networkAddressFactory";
+import {MasterWombatV2__factory} from "../../types/generated";
+import {getSigner} from "../utils";
+const fs = require('fs');
 
 task("timeTravel")
     .addParam("duration", "Length of time travel")
@@ -21,3 +24,23 @@ task("timeTravel")
         block = await ethers.provider.getBlock(blocknumber);
         console.log("new timestamp:", block.timestamp);
     });
+
+
+task("tt:wombat:pool").setAction(async function (taskArguments: TaskArguments, hre) {
+    const { ethers } = hre;
+    const deployer = await getSigner(hre);
+
+    const bnbConfig = JSON.parse(fs.readFileSync('./bnb.json', {encoding: 'utf8'}));
+    const masterWombat = MasterWombatV2__factory.connect(bnbConfig.masterWombat, deployer);
+
+    const pid = 0;
+    const pool = await masterWombat.poolInfo(pid);
+    console.log('pool', pool);
+    const user = await masterWombat.userInfo(pid, bnbConfig.voterProxy);
+    console.log('user', user, '\nrewardDebt', ethers.utils.formatEther(user.rewardDebt));
+    const pending = (user.amount.mul(pool.accWomPerShare).add(user.factor.mul(pool.accWomPerFactorShare)))
+            .div(ethers.BigNumber.from(1e12))
+            .add(user.pendingWom)
+            .sub(user.rewardDebt);
+    console.log('pending', pending);
+});
